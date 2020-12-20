04b6bcf7348f7a1ea3184e5de626065df90152a3
Vaibhav Krishnakumar <vk714@ic.ac.uk>
2020-12-20 19:37:58 +0000

    Implement Day 5 Part 2

--- day5/solution.scala
+++ day5/solution.scala
@@ -1,16 +1,24 @@
 import scala.io.Source
+import scala.collection.mutable.Set
 
 object Solution {
 
   val filename = "input.txt";
 
   def main(args: Array[String]) : Unit = {
-    val maxSeatId : Int = Source.fromFile(filename).getLines()
+    val remainingSeatIds : Set[Int] = generateAllSeatIds().iterator.to(Set);
+    Source.fromFile(filename).getLines()
       .map(toRowAndColumn)
-      .map(toNumbers)
-      .map(toSeatId)
-      .max;
-    println(s"Answer to Part 1 is $maxSeatId");
+      .map(Function.tupled(toNumbers _))
+      .map(Function.tupled(toSeatId _))
+      .foreach(remainingSeatIds.subtractOne);
+    val mySeatId : Int = findMySeatId(remainingSeatIds);
+    println(s"Answer to Part 2 is $mySeatId");
+  }
+
+  private def generateAllSeatIds() : Seq[Int] = {
+    for (r <- 0 until 128; c <- 0 until 8) // Range is end exclusive
+      yield(toSeatId(r, c))
   }
 
   private def toRowAndColumn(line: String) : (String, String) = {
@@ -18,10 +26,9 @@ object Solution {
     line.span(isRowDescriptor);
   }
 
-  private def toNumbers(rowColumnDescriptors: (String, String)) : (Int, Int) = {
-    val (rowDesc, columnDesc) = rowColumnDescriptors;
-    val row = toNumber(rowDesc, 'F', 'B');
-    val column = toNumber(columnDesc, 'L', 'R');
+  private def toNumbers(rowDescriptor: String, columnDescriptor: String) : (Int, Int) = {
+    val row = toNumber(rowDescriptor, 'F', 'B');
+    val column = toNumber(columnDescriptor, 'L', 'R');
     (row, column);
   }
 
@@ -30,9 +37,16 @@ object Solution {
     Integer.parseInt(zeroOneStr, 2);
   }
 
-  private def toSeatId(rowAndColumn: (Int, Int)) : Int = {
-    val (row, column) = rowAndColumn;
+  private def toSeatId(row: Int, column: Int) : Int = {
     row*8 + column;
   }
-}
 
+  private def findMySeatId(seats: Set[Int]) : Int = {
+    val mySeatId = seats.find(id => !seats.contains(id - 1) && !seats.contains(id + 1));
+    mySeatId match {
+      case Some(seatId) => seatId;
+      case None => throw new IllegalArgumentException(s"Couldn't find my seat in this set: $seats");
+    }
+  }
+
+}
