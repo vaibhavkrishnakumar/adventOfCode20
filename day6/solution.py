0103f7c63ddf20eb9b133c54b180bbb0be62ee39
Vaibhav Krishnakumar <vk714@ic.ac.uk>
2020-12-26 16:24:54 +0000

    Cleanup Day6

--- day6/solution.py
+++ day6/solution.py
@@ -1,22 +1,19 @@
 filename = "input.txt"
 
-def remove_all_whitespace(string):
-  return "".join(string.split())
+def anyone_answered_yes(each_persons_answers):
+  return len(reduce(set.union, each_persons_answers))
 
-def anyone_answered_yes(group):
-  return set(remove_all_whitespace(group))
-
-def everyone_answered_yes(group):
-  each_persons_answers = [set(person) for person in group.split()]
-  return reduce(set.intersection, each_persons_answers)
+def everyone_answered_yes(each_persons_answers):
+  return len(reduce(set.intersection, each_persons_answers))
 
 with open(filename, "r") as file:
   count_part_1 = 0
   count_part_2 = 0
   groups = file.read()
   for group in groups.split("\n\n"):
-    count_part_1 += len(anyone_answered_yes(group))
-    count_part_2 += len(everyone_answered_yes(group))
+    each_persons_answers = [set(person) for person in group.split()]
+    count_part_1 += anyone_answered_yes(each_persons_answers)
+    count_part_2 += everyone_answered_yes(each_persons_answers)
   print("Answer to Part 1 is {}".format(count_part_1))
   print("Answer to Part 2 is {}".format(count_part_2))
 
