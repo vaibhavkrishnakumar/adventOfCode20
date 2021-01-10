1744133e9277757fb5134d3118a2dd9156aff9aa
Vaibhav Krishnakumar <vk714@ic.ac.uk>
2021-01-10 18:10:52 +0000

    Implement Day 9 Part 2

--- day9/solution.groovy
+++ day9/solution.groovy
@@ -9,8 +9,8 @@ def findFirstNumberNotSummableInPreamble(input, preambleLength) {
     if (targetCannotBeFormedFromPreamble(preamble, target)) {
       return target
     }
- }
- throw new IllegalStateException("All numbers can be formed from the preamble")
+  }
+  throw new IllegalStateException("All numbers can be formed from the preamble")
 }
 
 def targetCannotBeFormedFromPreamble(preamble, target) {
@@ -25,11 +25,46 @@ def targetCannotBeFormedFromPreamble(preamble, target) {
   return true
 }
 
-def baseDir = '.'
-def filename = 'input.txt'
-def preambleLength = 25
+def findContiguousNumbersThatSumTo(target, input) {
+  def targets = [target]
+  for (int i = 1; i < input.size(); i++) {
+    def range = input[0..i]
+    def sumToI = range.sum()
+    if (targets.contains(sumToI)) {
+      return findSubRangeToEndThatSumsToTarget(range, target)
+    } else {
+      targets.add(targets[i-1] + input[i-1])
+    }
+  }
+  throw new IllegalStateException("No contiguous range sums to ${target}")
+}
 
-def input = readFileAsNumbers(baseDir, filename)
+// [0, 1, 2, 3...n] --> find a sub range that ends with n and sums to target
+def findSubRangeToEndThatSumsToTarget(range, target) {
+  for (int i = 0; i < range.size(); i++) {
+    def subRangeToEnd = range[i..range.size()-1]
+    if (subRangeToEnd.sum() == target) {
+      return subRangeToEnd
+    }
+  }
+  throw new IllegalArgumentException("Invalid range, no sub-range sums to ${target}")
+}
 
-def answer = findFirstNumberNotSummableInPreamble(input, preambleLength)
+def calculateMinAddMax(range) {
+  return range.min() + range.max()
+}
+
+// ---
+
+baseDir = '.'
+filename = 'input.txt'
+preambleLength = 25
+
+input = readFileAsNumbers(baseDir, filename)
+
+answer = findFirstNumberNotSummableInPreamble(input, preambleLength)
 println "Answer to Part 1 is ${answer}"
+
+range = findContiguousNumbersThatSumTo(answer, input)
+answer = calculateMinAddMax(range)
+println "Answer to Part 2 is ${answer}"
