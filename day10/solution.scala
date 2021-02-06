0e09c3c648ed058d6e8124fb7dbe5d8eeaf9365b
Vaibhav Krishnakumar <vk714@ic.ac.uk>
2021-02-06 18:05:12 +0000

    Implement Day 10 Part 2

--- day10/solution.scala
+++ day10/solution.scala
@@ -6,26 +6,68 @@ object Solution {
 
   def main(args: Array[String]) : Unit = {
     val adapters = Source.fromFile(filename).getLines().map(_.toInt).toSeq.sorted;
-    val (oneJumps, threeJumps) = adapterJumps(adapters);
-    val answerPart1 = oneJumps*threeJumps;
+    val adaptersWithSourceAndTarget = addSourceAndTarget(adapters)
+
+    val (singleJumps, tripleJumps) = adapterJumps(adaptersWithSourceAndTarget);
+    val answerPart1 = singleJumps*tripleJumps;
     println(s"Answer to Part 1 is $answerPart1");
+
+    val answerPart2 = numberOfCombinations(adaptersWithSourceAndTarget);
+    println(s"Answer to Part 2 is $answerPart2");
   }
 
-  def adapterJumps(adapters: Seq[Int]) : (Int, Int) = {
-    var oneJumps : Int = 0;
-    var threeJumps : Int = 1; // Final jump to device's built-in adapter
+  private def addSourceAndTarget(adapters: Seq[Int]) : Seq[Int] = {
+    val source: Int = 0
+    val target: Int = adapters.max + 3
+    (source +: adapters) :+ target
+  }
+
+  private def adapterJumps(adapters: Seq[Int]) : (Int, Int) = {
+    var singleJumps : Int = 0;
+    var tripleJumps : Int = 0;
 
     var current : Int = 0;
     adapters.foreach(jolt => {
       val jump = jolt - current;
       jump match {
-        case 1 => oneJumps = oneJumps + 1
-        case 3 => threeJumps = threeJumps + 1
+        case 1 => singleJumps = singleJumps + 1
+        case 3 => tripleJumps = tripleJumps + 1
+        case _ =>
       }
       current = jolt
     });
 
-    (oneJumps, threeJumps)
+    (singleJumps, tripleJumps)
   }
 
+  private def numberOfCombinations(adapters: Seq[Int]) : BigInt = {
+    val numWays = calculateNumberOfWaysForEachJolt(adapters)
+    return valueOfMaxKey(numWays)
+  }
+
+  private def calculateNumberOfWaysForEachJolt(adapters: Seq[Int]) : Map[Int, BigInt] = {
+    // Only one way of "reaching" 0
+    var numWays : Map[Int, BigInt] = Map(0 -> 1)
+
+    for (i <- 1 until adapters.length) {
+      val jolt : Int = adapters(i)
+      var numWaysForJolt : BigInt = getOrDefaultToZero(numWays, jolt - 1)
+      if (jolt > 1) {
+        numWaysForJolt += getOrDefaultToZero(numWays, jolt - 2)
+      }
+      if (jolt > 2) {
+        numWaysForJolt += getOrDefaultToZero(numWays, jolt - 3)
+      }
+      numWays = numWays + (jolt -> numWaysForJolt)
+    }
+    return numWays
+  }
+
+  private def getOrDefaultToZero(map: Map[Int, BigInt], key : Int) : BigInt = {
+    map.get(key).getOrElse(BigInt(0))
+  }
+
+  private def valueOfMaxKey(map: Map[Int, BigInt]) : BigInt = {
+    map.maxBy(_._1)._2
+  }
 }
